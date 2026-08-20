123fe278121e13b4384750255d5b68e2537330ef
Marzanul Islam <158351123+m4r24n@users.noreply.github.com>
2026-08-20 03:31:44 +0200

    Load selectable background themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./recurring-tasks.css";
 import "./archive-permanent-delete.css";
 import "./project-workspace-v2.css";
 import "./google-drive.css";
+import "./background-themes.css";
 
 export const metadata: Metadata = {
   title: "The Best Fucking Team",
